2b4f5deb6069dabea313973e89fcc607e9a84127
Vikal Dubey <208961904+Vikal-Dubey@users.noreply.github.com>
2026-07-29 00:11:03 +0530

    Updated database client

--- backend/db/prismaClient.js
+++ backend/db/prismaClient.js
@@ -1,10 +1,5 @@
-import "dotenv/config";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaNeon({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export { prisma };
